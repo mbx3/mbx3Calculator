e6c57dbc1d1cfb34029b595cdad1fec7583ea8a7
Mbx3_Bzr <mbx3.bzr3@gmail.com>
2023-03-04 00:29:05 +0330

    add Node class to calculator module

--- Calculator/calculations.py
+++ Calculator/calculations.py
@@ -1,18 +1,32 @@
 
-def sum(a,b):
+def Osum(a,b):
     return a+b
 
-def sub(a,b):
+def Osub(a,b):
     return a-b
 
-def mul(a,b):
+def Omul(a,b):
     return a*b
 
-def div(a,b):
+def Odiv(a,b):
     return a/b
 
-def pow(a,b):
+def Opow(a,b):
     return a**b
 
-def rad(a,b=2):
+def Orad(a,b=2):
     return a**(1/b)
+
+OPERATOR_FUNCTIONS=[Osum,Osub,Omul,Odiv,Opow,Orad]
+
+class Node:
+    def __init__(self,operator,op_left,op_right) -> None:
+        self.operator=operator
+        self.op_left=op_left
+        self.op_right=op_right
+    
+    def calc_node(self):
+        l = self.op_left.calc_node() if type(self.op_left) == Node else self.op_left
+        r = self.op_right.calc_node() if type(self.op_right) == Node else self.op_right
+        return self.operator(l,r)
+        
