15b7ea4f0d18d1404583f13ba80c9a4f6bb887e3
Mbx3_Bzr <mbx3.bzr3@gmail.com>
2023-03-06 13:16:41 +0330

    add operator_to_function and calculate functions to the calculations.py file

--- Calculator/calculations.py
+++ Calculator/calculations.py
@@ -1,3 +1,5 @@
+from string_work import *
+import re
 
 def Osum(a,b):
     return a+b
@@ -29,4 +31,24 @@ class Node:
         l = self.op_left.calc_node() if type(self.op_left) == Node else self.op_left
         r = self.op_right.calc_node() if type(self.op_right) == Node else self.op_right
         return self.operator(l,r)
-        
+
+def operator_to_function(operator:str):
+    if operator == '+' : return Osum        
+    if operator == '-' : return Osub        
+    if operator == '*' : return Omul        
+    if operator == '/' : return Odiv        
+    if operator == '^' : return Opow        
+    if operator == '$' : return Orad        
+
+def calculate(txt:str):
+    while True:
+        lastTxt = ''
+        while txt != lastTxt:
+            lastTxt = txt
+            txt=remove_unnecessary_parentheses(txt)
+        fppIndex, fpp = first_priority_parenthes(txt)
+        fpeIndex, fpe = first_priority_expression(fpp)
+        new_txt = Node(operator_to_function(fpe[1]),float(fpe[0]),float(fpe[2])).calc_node() if type(fpe) == tuple else fpe
+        txt = txt[:fppIndex[0]+fpeIndex[0]] + str(new_txt) + txt[fppIndex[0]+1+fpeIndex[1]:]
+        if is_calculation_end(txt): return txt
+
