9d4a86b5c10346f60b44a2a00b039629f15391ec
Mbx3_Bzr <mbx3.bzr3@gmail.com>
2023-03-10 16:06:00 +0330

    change the structure of the gui_tkinter.py file to object oriented

--- gui_tkinter.py
+++ gui_tkinter.py
@@ -4,106 +4,110 @@ from ttkthemes import ThemedTk,THEMES
 from functools import partial
 from Calculator import calculations
 
+class GUI:
+    BUTTONS_TEXT=[['(',')','C','<--'],
+                ['N','^','√','+'],
+                ['1','2','3','-'],
+                ['4','5','6','×'],
+                ['7','8','9','÷'],
+                ['Setting','0','.','=']]
 
-BUTTONS_TEXT=[['(',')','C','<--'],
-              ['N','^','√','+'],
-              ['1','2','3','-'],
-              ['4','5','6','×'],
-              ['7','8','9','÷'],
-              ['Setting','0','.','=']]
-
-def add_text(event):
-    if type(event) == str: 
-        calculationsText.set(calculationsText.get()+event)
-    elif event.char in '1234567890-+*/$^':
-        if event.char == '*': char = '×'
-        elif event.char == '/': char = '÷'
-        elif event.char == '$': char = '√'
-        else: char = event.char
-        calculationsText.set(calculationsText.get()+char)
-    
-
-def delete_char(event=None):
-    calculationsText.set(calculationsText.get()[:-1])
-
-def clear_text():
-    calculationsText.set('')
-
-def change_frame(frame:ttk.Frame):
-    frame.tkraise()
-
-def calculate():
-    try:
-        calculationsText.set(calculations.calculate(calculationsText.get()))
-    except ValueError:
-        calculationsText.set("Error!")
-
-def change_theme(theme):
-    root.config(theme=theme)
-    root.minsize(300, 500)
-    root.maxsize(400, 700)
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(0, weight=1)
-    mainFrame.rowconfigure(0, weight=5)
-    for row in range(1,7):
-        mainFrame.rowconfigure(row, weight=1)
-    for column in range(4):
-        mainFrame.columnconfigure(column, weight=1)
-
-root = ThemedTk(theme='adapta')
-root.configure(width=300,height=500)
-root.minsize(300, 500)
-root.maxsize(400, 700)
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-style = ttk.Style()
+    def __init__(self):
+        # Create and configure Root
+        self.root = ThemedTk(theme='adapta')
+        self.root.configure(width=300,height=500)
+        self.root.minsize(300, 500)
+        self.root.maxsize(400, 700)
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        style = ttk.Style()
 
 
-themesFrame = ttk.Frame(root)
-themesFrame.grid(row=0,column=0,sticky=tk.NSEW)
-themesFrame.grid_columnconfigure((0,1,2,3),weight=1)
-themesFrame.grid_rowconfigure((0,1),weight=1)
-ttk.Label(themesFrame,text='Theme : ').grid(row=0,column=0,padx=10,pady=5,sticky='nwe')
-theme = tk.StringVar()
-ttk.OptionMenu(themesFrame,theme,'adapta',*THEMES,command=lambda x: change_theme(x)).grid(row=0,column=1,padx=10,pady=5,columnspan=3,sticky='nwe')
-ttk.Button(themesFrame,text='OK',command=lambda : change_frame(mainFrame)). grid(row=1,column=1,pady=5,columnspan=2,sticky='swe')
+        self.themesFrame = ttk.Frame(self.root)
+        self.themesFrame.grid(row=0,column=0,sticky=tk.NSEW)
+        self.themesFrame.grid_columnconfigure((0,1,2,3),weight=1)
+        self.themesFrame.grid_rowconfigure((0,1),weight=1)
+        ttk.Label(self.themesFrame,text='Theme : ').grid(row=0,column=0,padx=10,pady=5,sticky='nwe')
+        theme = tk.StringVar()
+        ttk.OptionMenu(self.themesFrame,theme,'adapta',*THEMES,command=lambda x: self.change_theme(x)).grid(row=0,column=1,padx=10,pady=5,columnspan=3,sticky='nwe')
+        ttk.Button(self.themesFrame,text='OK',command=lambda : self.change_frame(self.mainFrame)). grid(row=1,column=1,pady=5,columnspan=2,sticky='swe')
+
+
+
+        self.mainFrame = ttk.Frame(self.root)
+        self.mainFrame.grid(row=0,column=0,sticky=tk.NSEW)
+
+        self.mainFrame.rowconfigure(0, weight=5)
+        for row in range(1,7):
+            self.mainFrame.rowconfigure(row, weight=1)
+        for column in range(4):
+            self.mainFrame.columnconfigure(column, weight=1)
+
+
+
+        self.calculationsText = tk.StringVar()
+        self.label = ttk.Label(self.mainFrame,textvariable=self.calculationsText)
+        self.label.grid(column=0,row=0,padx=2,pady=5,columnspan=4,sticky=tk.NSEW)
+        for i in range(4):
+            for j in range(1,7):
+                txt = GUI.BUTTONS_TEXT[j-1][i]
+                if txt == 'Setting' : btn_func = partial(self.change_frame,self.themesFrame)
+                elif txt == 'C' : btn_func = self.clear_text
+                elif txt == '<--': btn_func = self.delete_char
+                elif txt == '=': btn_func = self.calculate
+                else: btn_func = partial(self.add_text,txt)
+                btn = ttk.Button(self.mainFrame,text=txt,command=btn_func)
+                btn.grid(column=i,row=j,padx=2,pady=2,sticky=tk.NSEW)
 
 
 
 
 
-mainFrame = ttk.Frame(root)
-mainFrame.grid(row=0,column=0,sticky=tk.NSEW)
+        self.root.bind('<Key>',self.add_text)
+        self.root.bind('<BackSpace>',self.delete_char)
 
-mainFrame.rowconfigure(0, weight=5)
-for row in range(1,7):
-    mainFrame.rowconfigure(row, weight=1)
-for column in range(4):
-    mainFrame.columnconfigure(column, weight=1)
+        self.label.bind('<Configure>', lambda x: self.label.config(wraplength=self.label.winfo_width()))
 
 
+        self.root.mainloop()    
 
-calculationsText = tk.StringVar()
-label = ttk.Label(mainFrame,textvariable=calculationsText)
-label.grid(column=0,row=0,padx=2,pady=5,columnspan=4,sticky=tk.NSEW)
-for i in range(4):
-    for j in range(1,7):
-        txt = BUTTONS_TEXT[j-1][i]
-        if txt == 'Setting' : btn_func = partial(change_frame,themesFrame)
-        elif txt == 'C' : btn_func = clear_text
-        elif txt == '<--': btn_func = delete_char
-        elif txt == '=': btn_func = calculate
-        else: btn_func = partial(add_text,txt)
-        btn = ttk.Button(mainFrame,text=txt,command=btn_func)
-        btn.grid(column=i,row=j,padx=2,pady=2,sticky=tk.NSEW)
+    def add_text(self,event):
+        if type(event) == str: 
+            self.calculationsText.set(self.calculationsText.get()+event)
+        elif event.char in '1234567890-+*/$^':
+            if event.char == '*': char = '×'
+            elif event.char == '/': char = '÷'
+            elif event.char == '$': char = '√'
+            else: char = event.char
+            self.calculationsText.set(self.calculationsText.get()+char)
+        
 
+    def delete_char(self,event=None):
+        self.calculationsText.set(self.calculationsText.get()[:-1])
 
+    def clear_text(self):
+        self.calculationsText.set('')
 
+    def change_frame(self,frame:ttk.Frame):
+        frame.tkraise()
 
+    def calculate(self):
+        try:
+            self.calculationsText.set(calculations.calculate(self.calculationsText.get()))
+        except ValueError:
+            self.calculationsText.set("Error!")
 
-root.bind('<Key>',add_text)
-root.bind('<BackSpace>',delete_char)
+    def change_theme(self,theme):
+        self.root.config(theme=theme)
+        self.root.minsize(300, 500)
+        self.root.maxsize(400, 700)
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.mainFrame.rowconfigure(0, weight=5)
+        for row in range(1,7):
+            self.mainFrame.rowconfigure(row, weight=1)
+        for column in range(4):
+            self.mainFrame.columnconfigure(column, weight=1)
 
-label.bind('<Configure>', lambda x: label.config(wraplength=label.winfo_width()))
+GUI()    
 
-root.mainloop()
