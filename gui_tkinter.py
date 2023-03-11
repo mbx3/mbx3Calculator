97e19582e36986faf5abb26d831fbf0f10132785
Mbx3_Bzr <mbx3.bzr3@gmail.com>
2023-03-11 20:20:46 +0330

    change the way to change the theme and add suitable themes

--- gui_tkinter.py
+++ gui_tkinter.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-from ttkthemes import ThemedTk,THEMES
 from functools import partial
 from Calculator import calculations
 
@@ -11,40 +10,61 @@ class GUI:
                 ['4','5','6','×'],
                 ['7','8','9','÷'],
                 ['Setting','0','.','=']]
+    
+    COLOR_PALETTES={
+        'Light' : ['#FAEDCD','#FEFAE0','#E9EDC9','#A7727D'],
+        'Dark' : ['#18122B','#393053','#443C68','#635985'],
+        'Blue' : ['#62CDFF','#97DEFF','#C9EEFF','#AA77FF'],
+        'Red' : ['#AF0404','#FF0032','#FF0000','#252525'],
+        'Green' : ['#14C38E','#B8F1B0','#00FFAB','#E3FCBF'],
+        'Yellow' : ['#E7B10A','#F7F1E5','#898121','#4C4B16'],
+        'Jigili' : ['#F94A29','#FCE22A','#30E3DF','#D61355'],
+        'Gold' : ['#FFCE45','#D4AC2B','#B05E27','#7E370C'],
+        'Coffee' : ['#594545','#815B5B','#9E7676','#FFF8EA'],
+        'Warm' : ['#630606','#890F0D','#E83A14','#D9CE3F'],
+        'Cold' : ['#0E8388','#9E4784','#66347F','#37306B'],
+    }
 
     def __init__(self):
         # Create and configure Root
-        self.root = ThemedTk(theme='adapta')
+        self.root = tk.Tk()
         self.root.configure(width=300,height=500)
         self.root.minsize(300, 500)
         self.root.maxsize(400, 700)
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
-        style = ttk.Style()
+        self.style = ttk.Style()
+        self.style.theme_use('default')
+        self.change_theme('Dark')
 
-
+        # Create and configure Themes Frame
         self.themesFrame = ttk.Frame(self.root)
         self.themesFrame.grid(row=0,column=0,sticky=tk.NSEW)
         self.themesFrame.grid_columnconfigure((0,1,2,3),weight=1)
-        self.themesFrame.grid_rowconfigure((0,1),weight=1)
-        ttk.Label(self.themesFrame,text='Theme : ').grid(row=0,column=0,padx=10,pady=5,sticky='nwe')
+        self.themesFrame.grid_rowconfigure((0,1,2,3),weight=1)
         theme = tk.StringVar()
-        ttk.OptionMenu(self.themesFrame,theme,'adapta',*THEMES,command=lambda x: self.change_theme(x)).grid(row=0,column=1,padx=10,pady=5,columnspan=3,sticky='nwe')
-        ttk.Button(self.themesFrame,text='OK',command=lambda : self.change_frame(self.mainFrame)). grid(row=1,column=1,pady=5,columnspan=2,sticky='swe')
+        ttk.Label(self.themesFrame,text='Theme : ').grid(row=0,column=0,padx=10,pady=5,sticky='nwe')
+        for i,cp in enumerate(GUI.COLOR_PALETTES):
+            colors = GUI.COLOR_PALETTES[cp]
+            styleClass = cp+'.TRadiobutton'
+            self.style.configure(styleClass,background=colors[1],foreground=colors[3],indicatorcolor=colors[1])
+            self.style.map(styleClass,
+                    background=[('pressed', colors[2]),
+                                ('active', colors[1]),
+                                ('hover', colors[2]),
+                                ('selected', colors[0])],
+                    indicatorcolor=[('selected', colors[1]),
+                                    ('hover',colors[2])])
+            ttk.Radiobutton(self.themesFrame,style=styleClass,text=cp,variable=theme,value=cp,command=lambda x=cp: self.change_theme(x)).grid(row=(i+1)//4,column=(i+1)%4,padx=10,pady=5,sticky='nwe')
+        ttk.Button(self.themesFrame,text='OK',command=lambda : self.change_frame(self.mainFrame)). grid(row=3,column=1,pady=5,columnspan=2,sticky='swe')
 
 
-
+        # Create and configure Main Frame
         self.mainFrame = ttk.Frame(self.root)
         self.mainFrame.grid(row=0,column=0,sticky=tk.NSEW)
-
         self.mainFrame.rowconfigure(0, weight=5)
-        for row in range(1,7):
-            self.mainFrame.rowconfigure(row, weight=1)
-        for column in range(4):
-            self.mainFrame.columnconfigure(column, weight=1)
-
-
-
+        self.mainFrame.rowconfigure(list(range(1,7)), weight=1)
+        self.mainFrame.columnconfigure(list(range(4)), weight=1)
         self.calculationsText = tk.StringVar()
         self.label = ttk.Label(self.mainFrame,textvariable=self.calculationsText)
         self.label.grid(column=0,row=0,padx=2,pady=5,columnspan=4,sticky=tk.NSEW)
@@ -62,13 +82,12 @@ class GUI:
 
 
 
-
+        # Bind functions to widgets
         self.root.bind('<Key>',self.add_text)
         self.root.bind('<BackSpace>',self.delete_char)
-
         self.label.bind('<Configure>', lambda x: self.label.config(wraplength=self.label.winfo_width()))
 
-
+        # Start GUI main loop
         self.root.mainloop()    
 
     def add_text(self,event):
@@ -97,17 +116,17 @@ class GUI:
         except ValueError:
             self.calculationsText.set("Error!")
 
-    def change_theme(self,theme):
-        self.root.config(theme=theme)
-        self.root.minsize(300, 500)
-        self.root.maxsize(400, 700)
-        self.root.columnconfigure(0, weight=1)
-        self.root.rowconfigure(0, weight=1)
-        self.mainFrame.rowconfigure(0, weight=5)
-        for row in range(1,7):
-            self.mainFrame.rowconfigure(row, weight=1)
-        for column in range(4):
-            self.mainFrame.columnconfigure(column, weight=1)
+    def change_theme(self,color_palette):
+        color_palette = GUI.COLOR_PALETTES[color_palette]
+        self.style.configure('TFrame',background=color_palette[0])
+        self.style.configure('TLabel',background=color_palette[0],foreground=color_palette[3],font=('arial',11))
+        self.style.configure('TButton',activebackground=color_palette[0],background=color_palette[0],foreground=color_palette[3],focuscolor=color_palette[0],font=('arial',11))
+        self.style.map('TButton',
+                background=[('pressed', color_palette[2]),
+                            ('active', color_palette[1]),
+                            ('hover', color_palette[2]),
+                            ('selected', color_palette[0])])
+        
 
 GUI()    
 
